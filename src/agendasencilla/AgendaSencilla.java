d9a61a429e9efdf74e4a6a7b4a91c7cf46bc203c
Asus <Asus@DESKTOP-T2OO2E7>
2025-10-03 10:22:38 -0500

    Punto 1 y 2

---
+++ src/agendasencilla/AgendaSencilla.java
@@ -0,0 +1,9 @@
+package agendasencilla;
+
+public class AgendaSencilla {
+
+    public static void main(String[] args) {
+        Pantalla p = new Pantalla();
+        p.setVisible(true);
+    }    
+}
